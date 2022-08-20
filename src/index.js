14650997b60e8cbd8bdaf61b84bb975b4a746b24
Gael Souleymane Nongnogo <gmster22@gmail.com>
2022-08-19 22:51:14 -0400

    Add serviceWorker

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./containers/App"
 import reportWebVitals from './reportWebVitals';
 import "tachyons";
 
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -16,3 +18,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
